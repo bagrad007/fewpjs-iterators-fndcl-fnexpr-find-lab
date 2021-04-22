0ebcf76165261f2f46fe731e60a9dc81168ef6ff
Luke Gill <bagrad007@gmail.com>
2021-04-22 13:05:12 -0400

    commit tree 6eccacc1c02282a573ea237c22491b6b2d8fcf34

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const record = [
+    { year: "2018", result: "L" },
+    { year: "2017", result: "W" },
+    { year: "2016", result: "N/A" }
+]
 
+function superbowlWin(arr) {
+    let newArr = arr.find(function (s) {
+        let win = s.result === "W"
+        if (win) {
+            return s.year
+        } else {
+            undefined
+        }
+
+    })
+    if (newArr === undefined) {
+        return undefined
+    } else {
+        return newArr.year
+    }
+}
